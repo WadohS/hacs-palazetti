85afe09520428dfcef1143b2deefdbe9a0e3630e
WadohS <112435749+WadohS@users.noreply.github.com>
2025-12-01 23:48:57 +0100

    Delete tests directory

--- tests/const.py
+++
@@ -1,5 +0,0 @@
-"""Constants for integration_blueprint tests."""
-from custom_components.palazzetti.const import CONF_HOST
-
-# Mock config data to be used across multiple tests
-MOCK_CONFIG = {CONF_HOST: "192.168.99.99"}
